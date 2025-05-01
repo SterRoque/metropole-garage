4ee7cfff2f4609c8e4859e2ee357c5dbfd0025b2
Ster Roque <stermiss2@gmail.com>
2025-05-01 19:25:13 -0300

    chore: add knexfile config

---
+++ server/knexfile.ts
@@ -0,0 +1,14 @@
+import type { Knex } from "knex";
+import { env } from "./src/env/index.ts";
+
+export const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: "mysql2",
+    connection: env.DATABASE_URL,
+    migrations: {
+      directory: "./db/migrations",
+    },
+  },
+};
+
+export default config;
