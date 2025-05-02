5a57fd045ca425bb8a32053fb304c98f89a2bb0d
Ster Roque <stermiss2@gmail.com>
2025-05-02 03:49:26 -0300

    feat: register vehicles controller event listeners

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,9 @@
-import { playerController } from "@controllers/player-controller";
+import { playersController } from "@controllers/players-controller";
+import { vehiclesController } from "@controllers/vehicles-controller";
 
-on("playerConnecting", playerController.register);
+on("playerConnecting", playersController.register);
+
+onNet("garage:requestVehicles", vehiclesController.list);
+onNet("garage:spawnGarageVehicle", vehiclesController.spawn);
+
+onNet("garage:adminSpawnVehicle", vehiclesController.adminSpawn);
